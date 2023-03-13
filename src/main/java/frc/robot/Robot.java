57df5c36322e85626df2ac06754b579768a9a0f1
Deven Speyrer <devenspeyrer@gmail.com>
2023-03-13 16:36:59 -0600

    myabe fix auto-balance

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -71,9 +71,7 @@ public class Robot extends TimedRobot {
 		chooser.addOption("drop (LOWER)->Go",
 				SimpleAuto.dropPiece(arm, gripper, Level.BOTTOM).andThen(SimpleAuto.go(swerve)));
 
-		chooser.addOption("drop->Go", SimpleAuto.dropPiece(arm, gripper, Level.TOP).andThen(SimpleAuto.go(swerve)));
-
-		chooser.addOption("Autobalance (UNTESTED)", SimpleAuto.autoBalanceUntested(swerve));
+		chooser.addOption("Drop->Go", SimpleAuto.dropPiece(arm, gripper, Level.TOP).andThen(SimpleAuto.go(swerve)));
 
 		SmartDashboard.putData("Auto Path", chooser);
 	}
